3f29671fe52e30ebc3691bbc24e4362c28240fb8
jde98 <ekrjs004@gmail.com>
2021-12-16 14:30:17 +0900

    반응형 만드는중

--- js/script.js
+++ js/script.js
@@ -1,9 +1,34 @@
-/헤더 메뉴 색변경 /
-$(document).on('scroll', function() {
-	if($(document).scrollTop()>100) {
-		$('header').removeClass('pale').addClass('thick');
+/* 머시기 */
+window.addEventListener('scroll', function() {
+	var scrollPos = document.documentElement.scrollTop;
+
+	if (scrollPos == 0) {
+		header.classList.remove('thick');
 	} else {
-		$('header').removeClass('thick').addClass('pale');
+		header.classList.add('thick');
 	}
 });
 
+/* 거시기 */
+$(document).ready(function (){
+	if(window.innerWidth <= 990){
+		$(".nav").hide();
+		$(".nav_list_button").show();
+	}
+
+	/* 모바일 네비게이션바 버튼 */
+	$(".nav_list_button").click(function(){
+		$(".nav").slideToggle();
+	});
+
+	/* 모바일 or 데탑 사이징 별 메뉴  */
+	$(window).resize(function(){
+		if (window.innerWidth >= 991) {
+			$(".nav").show();
+			$(".nav_list_button").hide();
+		} else {
+			$(".nav").hide();
+			$(".nav_list_button").show();
+		}
+	});
+});
